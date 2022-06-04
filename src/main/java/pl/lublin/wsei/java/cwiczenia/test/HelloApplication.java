62a04e6c7ad7630a90426d0a6850e60b4e5f2129
37095studentwsei <37095@studentwsei.pl>
2022-06-04 20:59:36 +0200

    Realizacja podpunktu g

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/HelloApplication.java
@@ -0,0 +1,32 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gusInfoGraphic.fxml"));
+        FXMLLoader loader;
+        Parent root = loader.load();
+        ModuleLayer.Controller controller = loader.getController();
+
+        controller.setHostServices(this.getHostServices());
+        Object primaryStage;
+        controller.setStage(primaryStage);
+
+        Scene scene = new Scene(fxmlLoader.load(), 800, 700);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
