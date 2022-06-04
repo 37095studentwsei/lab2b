ca9f45ee9f03718155acb48d9563f42bbc6929cf
37095studentwsei <37095@studentwsei.pl>
2022-06-04 17:35:50 +0200

    .

--- src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestGusInfoGraphicList.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestGusInfoGraphicList.java
@@ -1,8 +1,5 @@
 package pl.lublin.wsei.java.cwiczenia.test;
 
-import pl.lublin.wsei.java.cwiczenia.test.GusInfoGraphicList;
-import pl.lublin.wsei.java.cwiczenia.test.Infografika;
-
 public class TestGusInfoGraphicList {
     public static void main(String[] args) {
         GusInfoGraphicList list = new GusInfoGraphicList("src/main/java/pl/lublin/wsei/java/cwiczenia/gusInfoGraphic.xml");
